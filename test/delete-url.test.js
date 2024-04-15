e3de9aa41a12f6e4933a8070801cf556c24e0297
Tendai Nyandoro <tnyandoro@gmail.com>
2024-04-15 08:09:16 +0200

    Add more specs for presents of urls

--- test/delete-url.test.js
+++ test/delete-url.test.js
@@ -2,19 +2,15 @@ const { db, server, request } = require('./config');
 
 describe('delete urls/:id', () => {
     it('should delete a URL when the provided ID is valid', async () => {
-        // Arrange
         const urlToDelete = { id: 1, name: 'Example URL', url: 'https://www.example.com' };
         await db.runAsync('INSERT INTO urls (name, url) VALUES (?, ?)', [urlToDelete.name, urlToDelete.url]);
 
-        // Act
         const response = await request(server)
         .delete(`/urls/${urlToDelete.id}`)
         .send();
 
-        // Assert
         expect(response.status).toBe(204);
 
-        // Check that the URL was deleted
         const deletedUrl = await db.getAsync('SELECT * FROM urls WHERE id = ?', [urlToDelete.id]);
         expect(deletedUrl).toBeUndefined();
     });
--- test/get-urls.test.js
+++ test/get-urls.test.js
@@ -1,24 +1,64 @@
 const { db, server, request } = require('./config');
 
 describe('get urls', () => {
-
   it('when there is no urls in database, should return no urls', async () => {
-
     const response = await request(server)
       .get('/urls')
       .send();
-
     expect(response.status).toBe(200);
     expect(response.body.count).toBe(0);
     expect(response.body.items.length).toBe(0);
     expect(response.body.page).toBe(0);
     expect(response.body.size).toBe(5);
-
   });
 
-  // when there is 1 urll in database, should return 1 url
-  // when there are 2 urls in database, should return 2 urls
-  // scenarios:
-  // - size, page, search, order (alphabetically order by name)
+  it('when there is 1 url in database, should return 1 url', async () => {
+    // Arrange
+    const urlToInsert = { name: 'Example URL', url: 'https://www.example.com' };
+    await db.runAsync('INSERT INTO urls (name, url) VALUES (?, ?)', [urlToInsert.name, urlToInsert.url]);
+    const insertedUrl = await db.getAsync('SELECT * FROM urls ORDER BY id DESC LIMIT 1');
 
+    // Act
+    const response = await request(server)
+      .get('/urls')
+      .send();
+
+    // Assert
+    expect(response.status).toBe(200);
+    expect(response.body.count).toBe(1);
+    expect(response.body.items.length).toBe(1);
+    expect(response.body.items[0].id).toBe(insertedUrl.id);
+    expect(response.body.items[0].name).toBe(insertedUrl.name);
+    expect(response.body.items[0].url).toBe(insertedUrl.url);
+    expect(response.body.page).toBe(0);
+    expect(response.body.size).toBe(5);
+  });
+
+  it('when there are 2 urls in database, should return 2 urls', async () => {
+    // Arrange
+    const url1 = { name: 'URL 1', url: 'https://www.example.com/1' };
+    const url2 = { name: 'URL 2', url: 'https://www.example.com/2' };
+    await db.runAsync('INSERT INTO urls (name, url) VALUES (?, ?)', [url1.name, url1.url]);
+    await db.runAsync('INSERT INTO urls (name, url) VALUES (?, ?)', [url2.name, url2.url]);
+    const url1Result = await db.getAsync('SELECT * FROM urls WHERE name = ?', [url1.name]);
+    const url2Result = await db.getAsync('SELECT * FROM urls WHERE name = ?', [url2.name]);
+
+    // Act
+    const response = await request(server)
+      .get('/urls')
+      .send();
+
+    // Assert
+    expect(response.status).toBe(200);
+    expect(response.body.count).toBe(2);
+    expect(response.body.items.length).toBe(2);
+    expect(response.body.items[0].id).toBe(url1Result.id);
+    expect(response.body.items[0].name).toBe(url1Result.name);
+    expect(response.body.items[0].url).toBe(url1Result.url);
+    expect(response.body.items[1].id).toBe(url2Result.id);
+    expect(response.body.items[1].name).toBe(url2Result.name);
+    expect(response.body.items[1].url).toBe(url2Result.url);
+    expect(response.body.page).toBe(0);
+    expect(response.body.size).toBe(5);
+  });
 });
